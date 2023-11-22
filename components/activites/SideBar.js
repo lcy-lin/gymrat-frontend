0de0bdab9dcee5245d262e03a1a8fb4ae505577b
lcy-lin <linjoy6@gmail.com>
2023-11-22 16:18:57 +0800

    feat(activities): side bar

---
+++ components/activites/SideBar.js
@@ -0,0 +1,49 @@
+export default function SideBar(){
+    return(
+        <>
+            <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
+                <div className="mb-2 p-4">
+                    <h5 className="block antialiased tracking-normal font-sans text-xl font-semibold leading-snug text-gray-900">Activity Record Tags</h5>
+                </div>
+                <nav className="flex flex-col gap-1 min-w-[240px] p-2 font-sans text-base font-normal text-gray-700">
+                    <div role="button" tabindex="0" className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
+                        <div className="grid place-items-center mr-4">
+                            🏋️
+                        </div>
+                        Chest
+                    </div>
+                    <div role="button" tabindex="0" className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-blue-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
+                        <div className="grid place-items-center mr-4">
+                            🧗
+                        </div>
+                        Back
+                    </div>
+                    <div role="button" tabindex="0" className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-blue-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
+                        <div className="grid place-items-center mr-4">
+                            🦵
+                        </div>
+                        Legs
+                    </div>
+                    <div role="button" tabindex="0" className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-blue-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
+                        <div className="grid place-items-center mr-4">
+                            💪
+                        </div>
+                        Arms
+                    </div>
+                    <div role="button" tabindex="0" className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-blue-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
+                        <div className="grid place-items-center mr-4">
+                            🤷
+                        </div>
+                        Shoulders
+                    </div>
+                    <div role="button" tabIndex="0" className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-blue-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
+                        <div className="grid place-items-center mr-4">
+                            🌐
+                        </div>
+                        Others
+                    </div>
+                </nav>
+            </div>
+        </>
+    );
+}
---
+++ pages/activities/index.js
@@ -0,0 +1,10 @@
+import Header from "@/components/Header";
+import SideBar from "@/components/activites/SideBar";
+export default function Activities() {
+    return (
+        <div>
+            <Header />
+            <SideBar />
+        </div>
+    );
+}
