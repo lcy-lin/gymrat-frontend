93f6f98b2df63009e2daba277b773b8832177a95
lcy-lin <linjoy6@gmail.com>
2023-11-25 15:07:06 +0800

    feat(signin): /signin page with api connection

--- components/signin/SignIn.js
+++ components/signin/SignIn.js
@@ -1,27 +1,54 @@
 import Link from "next/link";
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import AlertMessages from "@/utils/alertMessages";
+import Swal from "sweetalert2";
+import {setCookie} from "nookies";
 
 export default function SignIn() {
+    const router = useRouter();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/users/signin`, {
+            email, password
+            },{
+                headers: {
+                "Content-Type": "application/json",
+            }}).then((res) => {
+                AlertMessages.success("Signed In successfully");
+                setCookie(null, "accessToken", res.data.data.access_token, {
+                    maxAge: 30 * 24 * 60 * 60,
+                    path: "/",
+                });
+                router.push('/dashboard');
+            }).catch((err) => {
+                AlertMessages.error(err.response.data.error);
+            });
+    }
     return (
         <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-            <form className="space-y-6" action="#">
+            <form className="space-y-6" action="#" onSubmit={handleSubmit}>
                 <h1 className="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white">
                     Welcome Back, 
                 </h1>
                 <span className="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white underline underline-offset-3 decoration-8 decoration-blue-400 dark:decoration-blue-600"> GymRat</span>
                 <div>
-                    <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
+                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
                 </div>
                 <div>
-                    <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
                 </div>
                 <div className="flex items-start">
                     <div className="flex items-start">
                         <div className="flex items-center h-5">
                             <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
                         </div>
-                        <label for="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+                        <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
                     </div>
                     <a href="#" className="ms-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
                 </div>
