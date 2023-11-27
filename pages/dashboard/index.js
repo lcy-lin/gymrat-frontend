41393829e4812020c3a2dd939ad02a7b383c1984
lcy-lin <linjoy6@gmail.com>
2023-11-27 14:46:39 +0800

    feat(dashboard): add three button control in /dashboard

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,18 +1,23 @@
+import { useState } from 'react';
 import Header from '@/components/Header';
 import Title from '@/components/dashboard/Title';
 import Sheet from '@/components/dashboard/Sheet';
 import Weight from '@/components/dashboard/Weight';
+import Meal from '@/components/dashboard/Meal';
+
 export default function DashBoard() {
+    const [records, setRecords] = useState('activity');
     return (
         <div className="dark:bg-gray-800 min-h-screen">
             <Header />
             <div className="flex flex-col">
                 <div className="self-center">
-                    <Title />
+                    <Title setRecords={setRecords}/>
                 </div>
-                <Sheet />
+                {records === 'weight' && <Weight />}
+                {records === 'activity' && <Sheet />}
+                {records === 'meal' && <Meal />}
             </div>
-            {/* <Weight /> */}
         </div>
     );
 }
