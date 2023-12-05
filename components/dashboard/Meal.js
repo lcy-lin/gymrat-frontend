e7aea55cd861f91d57ea9003cf2e507284ac8e87
lcy-lin <linjoy6@gmail.com>
2023-12-05 16:06:46 +0800

    perf(meal): add descriptions for adding meals

--- components/dashboard/Meal.js
+++ components/dashboard/Meal.js
@@ -14,8 +14,24 @@ export default function Meal() {
     const [loading, setLoading] = useState(false);
 
     const handleSubmit = (e) => {
-        setLoading(true);
         e.preventDefault();
+        
+        const mealDescription = mealRef.current.value.trim();
+        if (!mealDescription) {
+            AlertMessages.error("Please enter a valid description for your meal.");
+            return;
+        }
+
+        if (!selectedType) {
+            AlertMessages.error("Please select a meal type.");
+            return;
+        }
+
+        if (!date) {
+            AlertMessages.error("Please select a date.");
+            return;
+        }
+        setLoading(true);
         const dateString = getDateString(date);
         const data = {
             user_id: cookies.userId,
@@ -45,10 +61,13 @@ export default function Meal() {
                     type="text"
                     placeholder="30g oats and 3 eggs"
                     className="rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+                    required
                 />
+                <p className="dark:text-gray-400">Please enter a string containing food or drink items. If you wish to calculate a specific quantity, you may prefix a quantity before an item. For example, 3 tomatoes or 1lb beef brisket. If no quantity is specified, the default quantity is 100 grams. The string cannot exceed 1500 characters.</p>
+                <label className="font-semibold dark:text-white">When did you eat?</label>
                 <DateComponent setDate={setDate} />
                 <span>
-                    <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select meal type</label>
+                    <label htmlFor="countries" className="font-semibold dark:text-white">Select meal type</label>
                     <select
                         id="countries"
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"   
